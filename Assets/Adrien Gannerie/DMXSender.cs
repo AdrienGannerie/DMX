64183d76714794a8ff5d0e814ee05e099067fc9d
AdrienGannerie <adrien.gannerie@gmail.com>
2019-11-14 23:39:21 +0100

    Initialization Unity project

---
+++ Assets/Adrien Gannerie/DMXSender.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DMXSender : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
